99f1a8b6ad26b3fd000ccd9dafc57705a01a82ea
Frank B. Brokken <f.b.brokken@rug.nl>
2015-12-17 15:17:56 +0100

    preparing for version 3.01.03

--- gpg-remailer/remailer/setlog.cc
+++ gpg-remailer/remailer/setlog.cc
@@ -13,11 +13,8 @@ void Remailer::setLog()
     if (!d_arg.option(&level, 'L'))
         level = configField("logLevel");
 
-    if (level.empty())
-        level = LOGDEFAULT;
-
     d_log.open(name);
     chmod(name.c_str(), S_IRUSR | S_IWUSR);
 
-    d_log.setLevel(stoul(level));
+    d_log.setLevel(level.empty() ? LOGDEFAULT : stoul(level));
 }
--- gpg-remailer/remailer/setumask.cc
+++ gpg-remailer/remailer/setumask.cc
@@ -6,6 +6,9 @@ void Remailer::setUmask()
     size_t umaskValue = 077;
     if (d_arg.option(&umaskOpt, "umask"))
     {
+
+cerr << "umask = " << umaskOpt << '\n';
+
         umaskValue = stoul(umaskOpt, 0, 8);
         d_log << level(LOGDEFAULT) << "WARNING: umask setting changed to " <<
             oct << setfill('0') << setw(3) << umaskValue << 
