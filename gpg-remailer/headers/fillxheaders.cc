ab6ac4d45fd453139bfa3dbf10226ce1ace080b8
Frank B. Brokken <f.b.brokken@rug.nl>
2017-09-22 11:23:53 +0200

    added headers/fillxheaders.cc

---
+++ gpg-remailer/headers/fillxheaders.cc
@@ -0,0 +1,26 @@
+#include "headers.ih"
+
+void Headers::fillXheaders()
+{
+    if (not ArgConfig::instance().option('x'))
+        return;
+
+    auto iter = begin();
+
+                            // always use the official envelope
+    d_xHeaders = R"( -a "X-GPG-remailer-from: )" + 
+                        String::escape(iter->substr(iter->find(' '))) + '"';
+    
+    for (++iter; iter != end(); ++iter)     // visit all remaining headers
+    {
+        string const &hdr = *iter;
+
+        if (hdr.find("envelope") != string::npos)
+            d_xHeaders += R"( -a "X-GPG-remailer-envelope: )" + 
+                        String::escape(hdr.substr(hdr.find(' '))) + '"';
+
+        else if (hdr.find("From: ") != string::npos)
+            d_xHeaders += R"( -a "X-GPG-remailer-From: )" + 
+                        String::escape(hdr.substr(hdr.find(' '))) + '"';
+    }
+}
