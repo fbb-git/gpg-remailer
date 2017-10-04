ef2235d02adc56acfe03e6040ed43c033b466273
Frank B. Brokken <f.b.brokken@rug.nl>
2017-10-04 15:46:08 +0200

    Fixed erroneous Content-... headers in reencrypted e-mail

--- gpg-remailer/mailerbase/headers.cc
+++ gpg-remailer/mailerbase/headers.cc
@@ -4,11 +4,15 @@ string MailerBase::headers() const
 {
     string mime = d_headers.getHeader("MIME-Version");
 
-    string contentHdr{ d_headers.xHeaders() };
+    string contentHdr{ d_headers.xHeaders() };  // get the X-GPG-remailer
+                                                // headers
 
-    if (not mime.empty())
+cerr << __FILE__ " " << contentHdr << '\n';
+
+    if (not mime.empty())                       // add the mime-version
         contentHdr += R"( -a ")" + String::escape(mime) + '"';
 
+                                                // look for the Content- hdrs
     d_headers.setHeaderIterator("Content-", MailHeaders::CASE_INITIAL);
 
     for 
@@ -26,21 +30,27 @@ string MailerBase::headers() const
             ||
             strcasestr(headerCp, "Content-Disposition") == headerCp
         )
+            continue;                           // skip Content-Type and 
+                                                // disposition (containing 
+                                                // org. boundary)
+
+        while (true)
         {
-            while (true)
-            {
-                size_t pos0 = header.find('\n');    // any newline? 
-                if (pos0 == string::npos)           // no: done
-                    break;
+            size_t pos0 = header.find('\n');    // any newline? 
+            if (pos0 == string::npos)           // no: done
+                break;
                                                 // pos. of 1st non-blank 
                                                 // beyond the \n
-                size_t pos1 = header.find_first_not_of(" \t\n", pos0);
+            size_t pos1 = header.find_first_not_of(" \t\n", pos0);
+
                 // e.g. 'n x': pos0 = 0, pos1 = 2 -> erase 1 char
-                header.erase(pos0, pos1 - pos0 - 1);
-                header[pos0] = ' ';
-            }
-            contentHdr += R"( -a ")" + String::escape(header) + '"';
+            header.erase(pos0, pos1 - pos0 - 1);
+            header[pos0] = ' ';
         }
+
+        contentHdr += R"( -a ")" + String::escape(header) + '"';
+        
     }
     return contentHdr;
 }
+
