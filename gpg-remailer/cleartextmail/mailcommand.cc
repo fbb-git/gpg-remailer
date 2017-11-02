e389f95c4685ddcdbbaa61dbad0b25c28a6729c7
Frank B. Brokken <f.b.brokken@rug.nl>
2017-11-02 11:17:03 +0100

    removed leftover std::cerr call from cleartextmail/mailcommand.cc

--- gpg-remailer/cleartextmail/mailcommand.cc
+++ gpg-remailer/cleartextmail/mailcommand.cc
@@ -2,8 +2,6 @@
 
 string ClearTextMail::mailCommand(string const &recipient) const
 {
-std::cerr << __FILE__": headers: " << headers(&processHeaders) << '\n';
-
     return
         "/usr/bin/mail -s '" + subject() + "' "
                 "-a \"Reply-To: " + d_replyTo + "\" " +
